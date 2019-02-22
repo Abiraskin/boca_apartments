2d99e372cb9d34f862cc0f5bf3347b0ade3d4feb
Abi Raskin <abiraskin@gmail.com>
2019-02-22 02:43:44 +0000

    initial commit

---
+++ lib/boca_apartments.rb
@@ -0,0 +1,6 @@
+require "boca_apartments/version"
+
+module BocaApartments
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/boca_apartments/version.rb
@@ -0,0 +1,3 @@
+module BocaApartments
+  VERSION = "0.1.0"
+end
