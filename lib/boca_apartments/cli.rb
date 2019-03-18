ab9a0f7949c359be5cc847ea8ab241b57bf319f8
Abi Raskin <abiraskin@gmail.com>
2019-03-18 03:50:02 +0000

    Added attrubutes to scrape

--- lib/boca_apartments/cli.rb
+++ lib/boca_apartments/cli.rb
@@ -1,12 +1,14 @@
 class BocaApartment::CLI
-  attr_accessor :name, :bed, :bath, :sqaure_ft, :price
+
 
   def call
     list_floorplans
     menu
+    goodbye
   end
 
   def list_floorplans
+    puts "See below a list of apartment floorplans and styles, listed by name:"
     @floorplans = BocaApartments::CLI.list
     @floorplans.each.with.index(1) do |floorplan, i|
       puts "#{i}. #{floorplan.name} - #{floorplan.bed} - #{floorplan.bath} - #{floorplan.sqaure_ft} - #{floorplan.price}."
@@ -16,11 +18,12 @@ class BocaApartment::CLI
   def menu
     input = nil
     while input != "exit"
-      puts " Enter the number of the apartment you want more more inforation about or type exit to end search"
+      puts " Enter the number of the apartment you want more more inforation about or type list to display floorplan options or type exit:"
       input = gets.strip.downcase
 
       if input.to_i > 0
-        puts @floorplans[input.to_i-1]
+        selcted_floorplan = @flooorplans[input.to_i-1]
+        puts "#{selcted_floorplan.name} - #{selcted_floorplan.bed} - #{selcted_floorplan.bath} - #{selcted_floorplan.sqaure_ft} - #{selcted_floorplan.price}"
       elsif input == "list"
         list_floorplans
       else
--- lib/boca_apartments/scraper.rb
+++ lib/boca_apartments/scraper.rb
@@ -13,15 +13,14 @@ class BocaApartment::Scraper
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.miznercourtapts.com/home.aspx"))
 
+    floorplan = self.new
+    floorplan.name =
+    floorplan.bed =
+    floorplan.bath =
+    floorplan.sqaure_ft =
+    floorplan.price =
+
+    floorplan
   end
 
-
-
-module BocaApartments
-  class Scraper
-    def self.scrape
-
-      binding.pry
-    end
-  end
 end
